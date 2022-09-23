80fd2df2c646d90f743462c673b7b8e0a56a5551
Notdetected <37031932+Notdet3cted@users.noreply.github.com>
2022-09-23 09:46:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var host = process.env.HOST || '0.0.0.0';
 // Listen on a specific port via the PORT environment variable
 var port = process.env.PORT || 8080;
-
+// chnagelog
 var cors_proxy = require('cors-anywhere');
 cors_proxy.createServer({
     originWhitelist: [], // Allow all origins
